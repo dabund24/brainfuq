db0837a4bdae95ebc1dcb79f8f21f473b71fd567
dabund24 <dev@dabund24.de>
2026-05-15 23:26:36 +0200

    replace p gates before transpiling to gate basis

--- src/qiskit_to_brainfuq/basis_gates.py
+++ src/qiskit_to_brainfuq/basis_gates.py
@@ -1,4 +1,5 @@
 import qiskit as qk
+import numpy as np
 from qiskit.transpiler import Target
 from qiskit.circuit.library import (
     XGate,
@@ -6,6 +7,8 @@ from qiskit.circuit.library import (
     HGate,
     CHGate,
     TGate,
+    CPhaseGate,
+    PhaseGate,
 )
 from qiskit.circuit import Measure
 
@@ -16,6 +19,7 @@ def to_basis_gates(qc: qk.QuantumCircuit) -> qk.QuantumCircuit:
     :param qc: The circuit to transpile
     :return: The transpiled qiskit circuit
     """
+    qc_without_param_instructions = _replace_param_instructions(qc)
 
     target = Target(num_qubits=qc.num_qubits)
 
@@ -40,7 +44,56 @@ def to_basis_gates(qc: qk.QuantumCircuit) -> qk.QuantumCircuit:
     target.add_instruction(Measure(), single_qubit_map)
 
     return qk.transpile(
-        qc,
+        qc_without_param_instructions,
         target=target,
         optimization_level=0,
     )
+
+def _replace_param_instructions(qc: qk.QuantumCircuit):
+    """
+    Handle gates with parameters. Transpiler is too dumb for them
+    """
+    res = qc.copy_empty_like()
+        
+    for instr in qc.data:
+        gate = instr.operation
+        qubits = instr.qubits
+        
+        if isinstance(gate, PhaseGate):
+            if np.isclose(float(gate.params[0]), np.pi):
+                res.t(qubits[0])
+                res.t(qubits[0])
+                res.t(qubits[0])
+                res.t(qubits[0])
+                continue
+            if np.isclose(float(gate.params[0]), np.pi / 2):
+                res.t(qubits[0])
+                res.t(qubits[0])
+                continue
+            if np.isclose(float(gate.params[0]), np.pi / 4):
+                res.t(qubits[0])
+                continue
+            else:
+                raise Exception("Cannot represent P gate with θ ∉ { π/2, π/4 } in qiskit basis")
+
+        elif isinstance(gate, CPhaseGate):
+            ct = TGate().control()
+            if np.isclose(float(gate.params[0]), np.pi):
+                res.append(ct, [qubits[0], qubits[1]])
+                res.append(ct, [qubits[0], qubits[1]])
+                res.append(ct, [qubits[0], qubits[1]])
+                res.append(ct, [qubits[0], qubits[1]])
+                continue
+            if np.isclose(float(gate.params[0]), np.pi / 2):
+                res.append(ct, [qubits[0], qubits[1]])
+                res.append(ct, [qubits[0], qubits[1]])
+                continue
+            if np.isclose(float(gate.params[0]), np.pi / 4):
+                res.append(ct, [qubits[0], qubits[1]])
+                continue
+            else:
+                raise Exception("Cannot represent controlled P gate with θ ∉ { π/2, π/4 } in qiskit basis")
+
+        res.append(instr)
+
+    return res
