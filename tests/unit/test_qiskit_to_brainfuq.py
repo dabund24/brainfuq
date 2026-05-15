1f322cf4cb5aa96d73a62fbf7f84dbb2897ed599
dabund24 <dev@dabund24.de>
2026-05-16 00:14:38 +0200

    test qiskit to brainuq

---
+++ tests/unit/test_qiskit_to_brainfuq.py
@@ -0,0 +1,22 @@
+import qiskit as qk
+from qiskit.circuit.library import QFTGate, TGate
+from qiskit_to_brainfuq.qiskit_to_brainfuq import qiskit_to_brainfuq
+
+def test_all_gates():
+    qc= qk.QuantumCircuit(2, 1)
+    qc.x(1)
+    qc.h(1)
+    qc.t(0)
+    qc.cx(0, 1)
+    qc.ch(0, 1)
+    qc.append(TGate().control(), [1, 0])
+    qc.measure(1, 0)
+    bfqc = qiskit_to_brainfuq(qc)
+    assert bfqc == ";}*~{#}*{#}~#{;}:"
+
+def test_qft():
+    qc= qk.QuantumCircuit(3)
+    qc.append(QFTGate(3), [0, 1, 2])
+    qc = qc.decompose()
+    bfqc = qiskit_to_brainfuq(qc)
+    assert bfqc == "}}~#{;}#{;~}#{{;}#{;}#{;~#}}*#{{*#}}*"
