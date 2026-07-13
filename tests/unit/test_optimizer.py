b689d3bb2982557401df5c85a617b108b9c28c12
dabund24 <dev@dabund24.de>
2026-07-13 09:10:15 +0200

    fix tests ending with pointer moves

--- tests/unit/test_optimizer.py
+++ tests/unit/test_optimizer.py
@@ -3,10 +3,10 @@ from optimizer.optimizer import optimize_brainfuq
 def test_simple():
     bfqc = "}}{{}{}*}}{:}}"
     optimized_bfqc = optimize_brainfuq(bfqc)
-    assert optimized_bfqc == "}*}:"
+    assert optimized_bfqc == "}*}:}}"
 
 def test_qft():
     bfqc = "}}~#{;}#}{{;~}#{{}}{{;}#{}{{{}{}}{}}{;}#{;~#}}*#{{*#}}*{{}}{}}"
     optimized_bfqc = optimize_brainfuq(bfqc)
-    assert optimized_bfqc == "}}~#{;}#{;~}#{{;}#{;}#{;~#}}*#{{*#}}*"
+    assert optimized_bfqc == "}}~#{;}#{;~}#{{;}#{;}#{;~#}}*#{{*#}}*}"
 
