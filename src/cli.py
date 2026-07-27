0e5a59ecdfdcd241021205e0b6e8eca5c7055ebf
dabund24 <dev@dabund24.de>
2026-07-27 22:50:50 +0200

    add to-qiskit translator to cli

--- src/cli.py
+++ src/cli.py
@@ -1,7 +1,9 @@
 import argparse
+from qiskit import qpy
 from brainfuq_interpreter.brainfuq_interpreter import BrainfuqInterpreter
 from brainfuq_interpreter.brainfuq_to_qiskit_translator import QiskitBrainfuqInterpreter
 from brainfuq_interpreter.brainfuq_simulator import BrainfuqSimulator
+from qiskit_to_brainfuq.qiskit_to_brainfuq import qiskit_to_brainfuq
 
 
 def simulate_brainfuq(program: str, verbose: bool):
@@ -14,13 +16,26 @@ def simulate_brainfuq(program: str, verbose: bool):
     if verbose:
         print(f"\nQuantum State:\n{brainfuq_simulator}\n\nClassical tape:\n{classical_tape}")
 
-def translate_to_qiskit(program: str):
+def translate_to_qiskit(program: str, output_file: str | None):
     print("Translating Brainfuq to Qiskit Circuit")
     
     interpreter = BrainfuqInterpreter(QiskitBrainfuqInterpreter())
     
     qiskit_circuit, _ = interpreter.interpret_program(program)
     print(qiskit_circuit)
+
+    if output_file is not None:
+        with open(output_file, "wb") as file:
+            qpy.dump(qiskit_circuit, file)
+            print(f"Wrote {output_file}")
+
+def translate_from_qiskit(input_file: str):
+    print("Translating Qiskit Circuit to Brainfuq")
+
+    with open(input_file, "rb") as handle:
+        qc = qpy.load(handle)
+
+    print(qiskit_to_brainfuq(qc[0])) 
 
 
 def main():
@@ -65,20 +80,35 @@ Simulate a GHZ state, where the amount of qubits are read from stdin. Print the 
     simulate_parser.add_argument("program", type=str, help="the brainfuq program string inside quotes")
     simulate_parser.add_argument("-v", "--verbose", action="store_true", help="print the quantum state and classical tape after simulation")
 
-    qiskit_parser = subparsers.add_parser(
+    to_qiskit_parser = subparsers.add_parser(
         "to-qiskit",
-        help="translate a brainfuq program into a qiskit circuit. For additional help, run brainfuq to-qiskit -h",
-        description="""Translate a brainfuq program into a qiskit circuit.
+        help="translate a brainfuq program into a qiskit circuit and optionally serialize it. For additional help, run brainfuq to-qiskit -h",
+        description="""Translate a brainfuq program into a qiskit circuit and optionally serialize it to a qpy file.
 
 Example:
-brainfuq to-qiskit "~,-[#}*{:}-]:"
-Print a qiskit circuit generating a GHZ state with subsequent measure operations, where the amount of qubits are read from stdin.""",
+brainfuq to-qiskit -o ghz.qpy "~,-[#}*{:}-]:"
+Print a qiskit circuit generating a GHZ state with subsequent measure operations, where the amount of qubits are read from stdin.
+Afterwards, serialize the circuit to ghz.qpy""",
         formatter_class=argparse.RawTextHelpFormatter)
-    qiskit_parser.add_argument("program", type=str, help="the brainfuq program string inside quotes")
+    to_qiskit_parser.add_argument("-o", "--output", metavar="filename", help="serialize qiskit circuit to qpy file")
+    to_qiskit_parser.add_argument("program", type=str, help="the brainfuq program string inside quotes")
+
+    from_qiskit_parser = subparsers.add_parser(
+        "from-qiskit",
+        help="translate a serialized qiskit circuit into a brainfuq program (experimental). For additional help, run brainfuq from-qiskit -h",
+        description="""Translate a serialized qiskit circuit into a brainfuq program (experimental).
+
+Example:
+brainfuq from-qiskit ghz.qpy
+Deserialize ghz.py and print a brainfuq translation of the circuit.""",
+        formatter_class=argparse.RawTextHelpFormatter)
+    from_qiskit_parser.add_argument("filename", type=str, help="the filename of the qpy file to deserialize and translate into brainfuq")
 
     args = parser.parse_args()
 
     if args.command == "simulate":
         simulate_brainfuq(args.program, args.verbose)
     elif args.command == "to-qiskit":
-        translate_to_qiskit(args.program)
+        translate_to_qiskit(program=args.program, output_file=args.output)
+    elif args.command == "from-qiskit":
+        translate_from_qiskit(args.filename)
